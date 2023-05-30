7a7d0cdae51dd459f8a2522e9ed0575622a0d9db
Alexey Panfilov <ap83@list.ru>
2023-05-30 13:55:37 +0300

    Passport number length limit added to DTO

--- adapter/src/main/java/ru/aston/dto/request/EmployeeUpdateDto.java
+++ adapter/src/main/java/ru/aston/dto/request/EmployeeUpdateDto.java
@@ -27,20 +27,21 @@ public class EmployeeUpdateDto {
 
     @Schema(description = "New employee surname. Can't be null or empty")
     @NotBlank(message = "Surname must be not blank")
-    @Length(max = 30, message = "surname is too long, must be 30 characters max")
+    @Length(max = 30, message = "Surname is too long, must be 30 characters max")
     private String surname;
 
     @Schema(description = "New employee middlename. Not required")
-    @Length(max = 30, message = "middlename is too long, must be 30 characters max")
+    @Length(max = 30, message = "Middlename is too long, must be 30 characters max")
     private String middleName;
 
     @Schema(description = "New employee login. Can't be null or empty. Must be unique")
     @NotBlank(message = "Login must be not blank")
-    @Length(max = 30, message = "login is too long, must be 30 characters max")
+    @Length(max = 30, message = "Login is too long, must be 30 characters max")
     private String login;
 
     @Schema(description = "New employee passport data. Can't be null or empty")
     @NotBlank(message = "Passport number must be not blank")
+    @Length(max = 11, message = "Passport number is too long, must be 11 characters max")
     private String passport;
 
     @Schema(description = "New employee passport date issue. Can't be null or empty")
