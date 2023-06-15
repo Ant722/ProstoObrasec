2e8df25ab1ec7d13a54ef73c8867f88ef12abac8
Alexey Panfilov <ap83@list.ru>
2023-06-15 16:41:48 +0300

    unused method deleted

--- app/src/main/java/ru/aston/exception/EmployeeNotFoundException.java
+++ app/src/main/java/ru/aston/exception/EmployeeNotFoundException.java
@@ -8,13 +8,8 @@ public class EmployeeNotFoundException extends RuntimeException {
     @Serial
     private static final long serialVersionUID = 7250603195899742560L;
     public static final String EMPLOYEE_NOT_FOUND_BY_UUID = "Account for employee with UUID %s not found";
-    private static final String NOT_FOUND_BY_LOGIN_MESSAGE = "No employee found with current login: (%s)";
 
     public EmployeeNotFoundException(UUID uuid) {
         super(String.format(EMPLOYEE_NOT_FOUND_BY_UUID, uuid));
-    }
-
-    public EmployeeNotFoundException(String login) {
-        super(String.format(NOT_FOUND_BY_LOGIN_MESSAGE, login));
     }
 }
