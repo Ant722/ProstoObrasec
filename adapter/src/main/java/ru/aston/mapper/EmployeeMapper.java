2971c73f4f14554dbe8c9d4f2294e2d69eee2e9a
P_Troshenkov <wm.trosh@yandex.ru>
2023-06-16 15:33:59 +0300

    Fix mapper

--- adapter/src/main/java/ru/aston/mapper/EmployeeMapper.java
+++ adapter/src/main/java/ru/aston/mapper/EmployeeMapper.java
@@ -18,13 +18,11 @@ import java.util.List;
 @Mapper(componentModel = "spring", uses = {PaginationMapper.class})
 public interface EmployeeMapper {
 
-
     String DATE_PATTERN_FORMAT = "dd.MM.yyyy";
 
     @Mapping(source = "passportDateIssue", target = "passportDateIssue", dateFormat = DATE_PATTERN_FORMAT)
     EmployeeInformationDto mapEmployeeToEmployeeInformationDto(Employee employee);
 
-    default String mapInstantToStringDate(LocalDateTime localDateTime) {
     EmployeeShortInformationDto mapEmployeeToEmployeeShortInformationDto(Employee employee);
 
     List<EmployeeShortInformationDto> mapListEmployeeToListEmployeeShortInformationDto(List<Employee> employeeList);
@@ -33,12 +31,6 @@ public interface EmployeeMapper {
     @Mapping(target = "info", source = "page")
     SearchEmployeeResultDto mapPageToSearchEmployeeResultDto(Page<Employee> page);
 
-    default String mapInstantToStringDate(Instant instant) {
-        return DateTimeFormatter.ofPattern(DATE_PATTERN_FORMAT)
-                .withZone(ZoneId.systemDefault())
-                .format(localDateTime);
-    }
-
     @Mappings({
             @Mapping(source = "employeeUpdateDto.passport", target = "passportId"),
             @Mapping(source = "employeeUpdateDto.passportDateIssue", target = "passportDateIssue", dateFormat = DATE_PATTERN_FORMAT),
@@ -46,4 +38,10 @@ public interface EmployeeMapper {
             @Mapping(source = "employeeUpdateDto.roleDto.role", target = "role")
     })
     Employee mapEmployeeUpdateDtoToEmployee(EmployeeUpdateDto employeeUpdateDto);
+
+    default String mapInstantToStringDate(LocalDateTime localDateTime) {
+        return DateTimeFormatter.ofPattern(DATE_PATTERN_FORMAT)
+                .withZone(ZoneId.systemDefault())
+                .format(localDateTime);
+    }
 }
